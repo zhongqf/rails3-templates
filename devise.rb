5087dab799ccf6c795b5e880a2990e198dbd177f
Alberto Perdomo <alberto.perdomo@aentos.es>
2011-01-27 18:03:40 +0000

    devise: Don't generate admin model, fixed git commit line

--- devise.rb
+++ devise.rb
@@ -1,9 +1,13 @@
 gem 'devise', '>=1.1.2'
+# If using HAML:
+gem 'hpricot', '0.8.3', :group => :development
+gem 'ruby_parser', '2.0.5', :group => :development
+
 run "bundle install"
 
 generate "devise:install"
+generate "devise:views"
 generate "devise User"
-generate "devise Admin"
 
 git :add => '.'
-git :commit => 'm "Devise"'
+git :commit => '-m "Devise"'
