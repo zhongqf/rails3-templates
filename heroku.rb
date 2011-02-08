41db0d98dde514f4859ca4cb1fc5fe262cba8068
Alberto Perdomo <alberto.perdomo@aentos.es>
2011-02-08 16:03:17 +0000

    Heroku: support staging and production environments. Opt-out or decide which ones to setup.

--- heroku.rb
+++ heroku.rb
@@ -2,7 +2,11 @@ def heroku(cmd, arguments="")
   run "heroku #{cmd} #{arguments}"
 end
 
-app_name = ask "\r\n\r\nEnter the application name to use on heroku:"
+envs = ask "\r\n\r\nEnter the list of environments to deploy NOW on heroku separated by commas, e.g.: staging, production (Leave empty for none):"
+app_name = ask "\r\n\r\nEnter the application name to use on heroku:" unless envs.empty?
+
+gem "heroku"
+run "bundle install"
 
 file ".slugignore", <<-EOS.gsub(/^  /, '')
   *.psd
@@ -19,20 +23,28 @@ EOS
 # we commit the compiled stylesheets to the git repo
 # and tell compass to never compile
 append_file 'config/environments/production.rb', 'Sass::Plugin.options[:never_update] = true'
+run "cp config/environments/production.rb config/environments/staging.rb"
 gsub_file '.gitignore', /^public\/stylesheets$/, ''
 
 run "bundle exec compass compile --output-style compressed --force"
 
 git :add => '.'
-git :commit => "-a -m 'Add heroku .slugignore'"
+git :commit => "-a -m 'Configuration for heroku'"
 
-heroku :create, app_name
-heroku :"config:add", 'BUNDLE_WITHOUT="development:test"'
-heroku :"addons:add", "custom_domains:basic"
-heroku :"addons:add", "exceptional:basic"
-heroku :"addons:add", "newrelic:bronze"
-heroku :"addons:add", "cron:daily"
+envs.split(", ").each do |env|
+  next unless ["staging", "production"].include?(env)
+  heroku :create, "#{app_name}-#{env} --remote heroku-#{env}"
 
-git :push => "heroku master"
-heroku :rake, "db:migrate"
-heroku :open
+  heroku :"config:add", "BUNDLE_WITHOUT='development:test' --app #{app_name}-#{env}"
+  heroku :"config:add", "RACK_ENV=#{env} --app #{app_name}-#{env}"
+
+  # addons
+  heroku :"addons:add", "custom_domains:basic"
+  heroku :"addons:add", "exceptional:basic"
+  heroku :"addons:add", "newrelic:bronze"
+  heroku :"addons:add", "cron:daily"
+
+  git :push => "heroku-#{env} master"
+  heroku :rake, "db:migrate --app #{app_name}-#{env}"
+  heroku :open, "--app #{app_name}-#{env}"
+end
